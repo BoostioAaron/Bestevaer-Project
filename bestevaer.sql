82a2e06aae4d531f25cb1fad00551366b01d36a7
BoostioA <161090@student.horizon.nl>
2021-01-25 15:37:31 +0100

    final touches

--- bestevaer.sql
+++ bestevaer.sql
@@ -1,30 +1,25 @@
 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
--- Database: `bestevaer`
---
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `contact`
---
 
 CREATE TABLE `contact` (
-  `naam` varchar(50) DEFAULT NULL,
+  `voornaam` varchar(50) DEFAULT NULL,
   `achternaam` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `vraag` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Tabelstructuur voor tabel `schepen`
---
+
+INSERT INTO `contact` (`voornaam`, `achternaam`, `email`, `vraag`) VALUES
+('Aaron', ' laan', ' Aaron.laan@gmail.com', 'Hoeveel boten hebben jullie bij bestevaer?'),
+('Aaron', ' Random', ' Steen@gmail.com', '\r\nHoe werkt het berekenen?'),
+('Banaan', ' Banaan', ' Banaan@gmail.com', 'Hoeveel schepen zijn er om uit te kiezen?'),
+('Aaron', ' Laan', ' Aaronlaan@gmail.com', 'Wat is mijn naam?'),
+('Aaron', ' Banaan', ' Aaron@banaan.nl', 'WAT IS MIJN NAAM BANAAN?');
+
+
 
 CREATE TABLE `schepen` (
   `imono` int(10) NOT NULL,
@@ -35,22 +30,15 @@ CREATE TABLE `schepen` (
   `afbeelding` varchar(2048) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Gegevens worden geëxporteerd voor tabel `schepen`
---
 
 INSERT INTO `schepen` (`imono`, `naam`, `DWT`, `GT`, `volume`, `afbeelding`) VALUES
-(9071052, 'Hermes', 5809, 3806, 6633.9, NULL),
-(9071076, 'NSANGELA', 5714, 3806, 6633.9, NULL),
-(9123506, 'LuckyStar', 6800, 4178, 7527.7, NULL),
-(9311555, 'Triumph', 7150, 4039, 8189.1, NULL),
-(9414735, 'Sabrina', 10030, 6278, 12594.9, NULL);
+(9071052, 'Hermes', 5809, 3806, 6633.9, 'image/HermsSchip.jpg'),
+(9071076, 'NSANGELA', 5714, 3806, 6633.9, 'image/NS_ANGELASchip.JPG'),
+(9123506, 'LuckyStar', 6800, 4178, 7527.7, 'image/LUCKY_STARSchip.JPG'),
+(9311555, 'Triumph', 7150, 4039, 8189.1, 'image/TRIUMPHIVSchip.JPG'),
+(9414735, 'Sabrina', 10030, 6278, 12594.9, 'image/SABRINASchip.jpg');
 
--- --------------------------------------------------------
 
---
--- Tabelstructuur voor tabel `users`
---
 
 CREATE TABLE `users` (
   `usersId` int(11) UNSIGNED NOT NULL,
@@ -60,48 +48,32 @@ CREATE TABLE `users` (
   `usersPwd` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Gegevens worden geëxporteerd voor tabel `users`
---
+
 
 INSERT INTO `users` (`usersId`, `usersName`, `usersEmail`, `usersUid`, `usersPwd`) VALUES
 (1, 'Aaron', 'aaron.laan@gmail.com', 'Aaron', '$2y$10$AYTiE/VXfJvCUF01qAfyge/BUcng6ST48kbA4wguC5fbHQHE1qO5W'),
 (2, 'Lars Hekelaar', 'larshekelaar@gmail.com', 'Lars Hekelaar', '$2y$10$qwM5gNV8Amy..uOz9E04KuFuRNn8cMWTAvwbBSCPVGt2u8rb3Y83a'),
 (3, 'Yann', 'Yann@gmail.com', 'Yann', '$2y$10$oYPCB.vYsNq3UAKNP1zYleKFe3DWfrW4X2QWwv3/azTE5ESlBPo1O'),
-(4, 'Boostio', 'Boostio@gmail.com', 'Boostio', '$2y$10$I/UbC.OAC6OjbE1u3v1TBuMfZGSEEyhR4UnlIzbIkAuzSPwFYh.gG');
+(4, 'Boostio', 'Boostio@gmail.com', 'Boostio', '$2y$10$I/UbC.OAC6OjbE1u3v1TBuMfZGSEEyhR4UnlIzbIkAuzSPwFYh.gG'),
+(7, 'Arno laan', 'ditadres@gmail.com', 'Arno laan', '$2y$10$m7zaHWoL0UqI.n/yYJiNEOIfAyHc2Na2YVeu.DcRmT.fkrC.Yw/Ta'),
+(8, 'Yoshi', 'Yoshi@ninetendo.com', 'Yoshi', '$2y$10$xLtvMBK26CoKcE77cAaLROw9wzWtrWAzRkXWgCGIli.K4btt6XwLO');
 
---
--- Indexen voor geëxporteerde tabellen
---
 
---
--- Indexen voor tabel `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`achternaam`);
 
---
--- Indexen voor tabel `schepen`
---
+
 ALTER TABLE `schepen`
   ADD PRIMARY KEY (`imono`),
   ADD UNIQUE KEY `Naam` (`naam`);
 
---
--- Indexen voor tabel `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`usersId`);
 
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
 
---
--- AUTO_INCREMENT voor een tabel `users`
---
+
+
 ALTER TABLE `users`
-  MODIFY `usersId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `usersId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 
--- onzeSchepen.php
+++ onzeSchepen.php
@@ -13,24 +13,23 @@
  <div id="knoppeninfowrapper">
 <div id="knoppeninfoitems">
  <form action="" method="post">  
- <input type="submit" id="Hermesbtn" name="Hermesbtn" value="Hermes"><form>
+ <input type="submit" id="Hermesbtn" class="onzeschepenknop" name="Hermesbtn" value="Hermes"><form>
  <form action="" method="post">  
- <input type="submit" id="nsangelabtn" name="nsangelabtn" value="nsangela"><form>
+ 
+ <input type="submit" id="nsangelabtn" class="onzeschepenknop" name="nsangelabtn" value="nsangela"><form>
 
  <form action="" method="post">  
- <input type="submit" id="luckystarbtn" name="luckystarbtn" value="luckystar"><form>
-
+ <input type="submit" id="luckystarbtn" class="onzeschepenknop" name="luckystarbtn" value="luckystar"><form>
+ 
  <form action="" method="post">  
- <input type="submit" id="triumphbtn" name="triumphbtn" value="triumph"><form>
-
+ <input type="submit" id="triumphbtn" class="onzeschepenknop" name="triumphbtn" value="triumph"><form>
  <form action="" method="post">  
- <input type="submit" id="sabrinabtn" name="sabrinabtn" value="sabrina"><form>
- </div>  
+ <input type="submit" id="sabrinabtn"  class="onzeschepenknop" name="sabrinabtn" value="sabrina"><form>
+ <div>  
 </div>
  </main>
 
- <div id="infovanschepen">
-  
+ <div id="infovanschepen"> 
  <div id="infovanschepenitems">
  <?php
  require_once "includes/Infobutton.php";
